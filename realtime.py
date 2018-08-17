a2cc921937226795f41bc6f4e03997c5fbbdf253
Corey Lowman <clowman1993@gmail.com>
2018-08-17 16:35:04 -0400

    Commenting code

--- realtime.py
+++ realtime.py
@@ -3,34 +3,81 @@ import numpy
 from train import data
 
 
+# LSTM class for keeping track of all of the LSTM pieces of data
+# theres a big difference between how we will use this lstm and how we used the lstm in training:
+# for training, we passed a whole sequence of time steps in at once
+# for real time, we are only passing in 1 time step at a time
+#
+# therefore we need to keep track of the internal lstm state in between the time steps
+#
+# note: the intended use of this is for use in things like games or robotics... you are only getting 1 time step at a
+# time
 class RealTimeLSTM(object):
     def __init__(self, x, units):
+        """
+        :param x: input tensor
+        :param units: size of the lstm
+        """
         lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(units)
 
+        # here we initialize the placeholders for the internal state.
+        # we will need to put data into self.state_input when we call session.run()
         cell_state = tf.placeholder(tf.float32, [1, lstm_cell.state_size.c])
         hidden_state = tf.placeholder(tf.float32, [1, lstm_cell.state_size.h])
-        initial_state = tf.nn.rnn_cell.LSTMStateTuple(cell_state, hidden_state)
-
-        self.output, lstm_state = tf.nn.dynamic_rnn(lstm_cell, x, initial_state=initial_state)
         self.state_input = [cell_state, hidden_state]
+
+        # here is what we'll use initially to pass into self.state_input, and we'll use this when we want to reset
+        # the LSTMs state
         self.state_init = [numpy.zeros((1, lstm_cell.state_size.c), numpy.float32),
                            numpy.zeros((1, lstm_cell.state_size.h), numpy.float32)]
+
+        # wrap the two placeholders in a state tuple so we can pass it to the dynamic_rnn
+        initial_state = tf.nn.rnn_cell.LSTMStateTuple(cell_state, hidden_state)
+
+        # same way of initializing the dynamic_rnn as before BUT with one big difference:
+        # no need to pass in a sequence_length
+        self.output, lstm_state = tf.nn.dynamic_rnn(lstm_cell, x, initial_state=initial_state)
+
+        # this holds the new LSTM state after we invoke the rnn
+        # lstm_state is a tuple of (cell_state, hidden_state), each with the shape:
+        # (batch, data).
         self.state_out = (lstm_state[0][:1, :], lstm_state[1][:1, :])
 
 
+# our model
 class RealtimeModel(object):
     def __init__(self, scope):
+        # again we have to scope the variables the same way so loading works
+        # note: the setup of the model is exactly the same
         with tf.variable_scope(scope):
             self.input = tf.placeholder(tf.float32, [None, None, 1])
             self.lstm = RealTimeLSTM(self.input, 16)
             self.output = tf.layers.dense(self.lstm.output, 1)
 
+        # note: we don't have any loss functions or placeholders here, because we aren't training with this model
+
+        # we have to keep track of the LSTMs current state. this gets set when forward() is invoked later, and reset
+        # when reset_states() is called
         self.current_state = self.lstm.state_init
 
     def reset_states(self):
+        """
+        Resets the LSTMs state. Use when you are about to start a new sequence
+        :return:
+        """
         self.current_state = self.lstm.state_init
 
     def forward(self, session, x):
+        """
+        Run the model
+        :param session: tf.Session
+        :param x: input data to pass into self.input. needs to be of the shape [batch, time step, data]. here we
+        only pass in 1 batch and 1 time step at a time, so essentially we pass in [[data]].
+        :return: output of the network
+        """
+        # since we include both output & self.lstm.state_out in our fetches, this function returns both the output
+        # of the network and also the new state of the LSTM. we save it into self.current_state
+        # note: in the feed dict, we pass self.current_state into self.lstm.state_input
         output, self.current_state = session.run([self.output, self.lstm.state_out],
                                                  {self.input: x,
                                                   self.lstm.state_input[0]: self.current_state[0],
@@ -39,19 +86,32 @@ class RealtimeModel(object):
 
 
 def main():
+    # initialize the model
     model = RealtimeModel('seq-model')
 
+    # initialize a saver to load all the weights we saved in train.py
     variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'seq-model')
     saver = tf.train.Saver(variables)
 
     with tf.Session() as session:
+        # instead of initializing the weights with a `session.run(tf.global_variables_initializer())`, we load
+        # the weights using the saver
         saver.restore(session, 'model/model.ckpt')
 
+        # test the sequences
         for sequence in data:
+            # reset the internal LSTM state - we are starting a new sequence
             model.reset_states()
             print(sequence)
             for i, x in enumerate(sequence[:-1]):
-                print('{} -> {} (predicted {:.3f})'.format(x, sequence[i + 1], model.forward(session, [[[x]]])[0][0][0]))
+                # we expect it to predict the next number in the sequence
+                y = sequence[i + 1]
+
+                # here's where we invoke model.forward to get it's prediction
+                # note: how we wrap the input number to be of the shape [batch, timestep, data]
+                # note: how we unwrap the output of the model, which is in the shape [batch, timestep, data]
+                predicted_y = model.forward(session, [[[x]]])[0][0][0]
+                print('{} -> {} (predicted {:.3f})'.format(x, y, predicted_y))
             print('---')
 
 
--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
+import math
 
-# initializing data
+# initializing data, just a bunch of sequences of numbers made up of patterns.
+# this is what we want our RNN to learn to predict
 data = [
     [1, 2, 3, 4, 5, 6],
     [2, 4, 6, 8],
@@ -11,59 +13,103 @@ data = [
     [1, 1, 1, 1, 1, 1, 1]
 ]
 
+# the length of the longest sequence we have - used to pad our sequences
+# in tensorflow you have to pad variable length sequences during RNN training, because it rolls out the RNN
 MAX_SEQ_LEN = max(map(len, data))
 
+# these will be the variables that contain the padded data that are also of the shape:
+# [batch, timestep, data]
 X = []
 Y = []
+
+# we keep track of the original length of the sequences as well (before the padding). tensorflow can use this to
+# be more efficient, and so the model doesn't actually use the padded 0s
 SEQ_LENS = []
 
-# padding data
+# pad the data
 for i, sequence in enumerate(data):
     padded_sequence = sequence + [0] * (MAX_SEQ_LEN - len(sequence))
+
+    # the target values will just be the next element in the sequence, therefore the Ys are just offset by 1
     X.append([[v] for v in padded_sequence[:-1]])
     Y.append([[v] for v in padded_sequence[1:]])
+
+    # because we are removing an element when we add to X and Y, the sequence length is actually 1 smaller
     SEQ_LENS.append(len(sequence) - 1)
 
 
+# our LSTM class - mostly convenience, but also to mirror the realtime.py setup
 class TrainLSTM(object):
     def __init__(self, x, units):
+        """
+        :param x: the input tensor
+        :param units: the size of the LSTM
+        """
+        # seq_length will be what we stuff SEQ_LENS in to tell tensorflow the actual length of the padded sequence
         self.seq_length = tf.placeholder(tf.int32, shape=[None])
+
+        # standard dynamic_rnn initialization. note how we pass self.seq_length into sequence_length
         self.output, lstm_state = tf.nn.dynamic_rnn(tf.nn.rnn_cell.BasicLSTMCell(units), x, dtype=tf.float32,
                                                     sequence_length=self.seq_length)
 
 
+# convenience class - again mostly conveience and to mirror realtime.py setup
 class TrainModel(object):
     def __init__(self, scope):
+        # scope the variables so we can save & load them properly
         with tf.variable_scope(scope):
+            # input will have the shape [batch, timestep, data]. since our data is just 1 integer at a time, we put
+            # shape 1 for our data
             self.input = tf.placeholder(tf.float32, [None, None, 1])
+
+            # use our lstm conveience class
             self.lstm = TrainLSTM(self.input, 16)
+
+            # we are only outputting 1 number - the next number in the sequence
             self.output = tf.layers.dense(self.lstm.output, 1)
 
+        # these are our training tensors - target is where we will put our target predictions Y, and loss is how
+        # we compute the loss (just using mean squared error right now)
         self.target = tf.placeholder(tf.float32, [None, None, 1])
         self.loss = tf.reduce_mean(tf.square(self.output - self.target))
 
 
+# main method
 def train():
+    # initialize the model with the scope 'seq-model'. all variables will be under that scope
     model = TrainModel('seq-model')
+
+    # create an optimizer and capture the train_step we will use to train the model
     train_step = tf.train.AdamOptimizer().minimize(model.loss)
 
+    # create a saver so we can save off the weights of the network and use it in realtime.py
+    # note: not sure if passing in variables is necessary
     variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'seq-model')
     saver = tf.train.Saver(variables)
 
     with tf.Session() as session:
+        # initialize all the weights
         session.run(tf.global_variables_initializer())
 
-        loss = 2
-
+        # our main training loop - keeps going until our loss (mean squared error) gets below 1
         i = 0
+        loss = math.inf
         while loss > 1:
+            # compute the loss and run the training step.
+            # two things:
+            # 1. no batching is done here, we just pass in all of our data. this is because we have such a small data
+            #    set and this is a toy example
+            # 2. note how we have to pass in SEQ_LENS to the seq_length place holder
             loss, _ = session.run([model.loss, train_step],
                                   {model.input: X, model.target: Y, model.lstm.seq_length: SEQ_LENS})
             i += 1
             print(i, loss)
 
+        # save all the weights
         saver.save(session, 'model/model.ckpt')
 
+        # do some testing to show how we do - pass in part of the sequence and make sure the number it predicts is the
+        # next number in the sequence
         for sequence in X:
             for i in range(len(sequence) - 1):
                 if i == 0: continue
